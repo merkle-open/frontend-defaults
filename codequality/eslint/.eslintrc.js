bdb70f855d5d3d6ff075193a6e5fdbb6de8e22de
ernscht <ernscht@gmail.com>
2016-09-19 09:16:30 +0200

    [eslint] add globals

--- codequality/eslint/.eslintrc.js
+++ codequality/eslint/.eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-	'extends': '@namics/eslint-config/configurations/es6-browser.js'
+	'extends': '@namics/eslint-config/configurations/es6-browser.js',
+	'globals': {
+		'jQuery': true,
+		'T': true
+	}
 };
