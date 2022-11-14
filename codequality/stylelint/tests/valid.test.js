d31ecb872719e2f05e9429c7ca336be0715139b7
ernscht <ernscht@gmail.com>
2022-11-14 19:00:17 +0100

    test(stylelint-config): add more test files

--- codequality/stylelint/tests/valid.test.js
+++ codequality/stylelint/tests/valid.test.js
@@ -19,6 +19,16 @@ describe('flags no errors with valid css', () => {
 	it('flags a todo warning in slick.scss', () => {
 		expect(result.results[0].warnings).toHaveLength(0);
 		expect(result.results[1].warnings).toHaveLength(0);
-		expect(result.results[2].warnings).toHaveLength(1);
+		expect(result.results[2].warnings).toHaveLength(0);
+		expect(result.results[3].warnings).toHaveLength(0);
+		expect(result.results[4].warnings).toHaveLength(0);
+		expect(result.results[5].warnings).toHaveLength(0);
+		expect(result.results[6].warnings).toHaveLength(0);
+		expect(result.results[7].warnings).toHaveLength(0);
+		expect(result.results[8].warnings).toHaveLength(0);
+		expect(result.results[9].warnings).toHaveLength(0);
+		expect(result.results[10].warnings).toHaveLength(0);
+		expect(result.results[11].warnings).toHaveLength(1);
+		expect(result.results[12].warnings).toHaveLength(0);
 	});
 });
