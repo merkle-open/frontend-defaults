e1ff052f82103854d8817a76ff4796c7255ac929
ernscht <ernscht@gmail.com>
2022-11-18 16:13:25 +0100

    test(stylelint-config): update tests for new config

--- codequality/stylelint/tests/invalid.test.js
+++ codequality/stylelint/tests/invalid.test.js
@@ -17,13 +17,14 @@ describe('flags errors with invalid css', () => {
 	});
 
 	it('flags warnings', () => {
-		expect(result.results[0].warnings).toHaveLength(8);
+		expect(result.results[0].warnings).toHaveLength(9);
 	});
 
 	it('correct warning text', () => {
 		expect(result.results[0].warnings.map((w) => w.text)).toEqual([
 			'Expected class name "selector" to start with a valid prefix: "a-", "m-", "o-", "l-", "g-", "h-", "state-". (plugin/stylelint-bem-namics)',
 			'Expected class name "z-selector" to start with a valid prefix: "a-", "m-", "o-", "l-", "g-", "h-", "state-". (plugin/stylelint-bem-namics)',
+			'Expected modern color-function notation (color-function-notation)',
 			'Expected "#CCC" to be "#ccc" (color-hex-case)',
 			'Unexpected named color "darkgray" (color-named)',
 			'Unexpected !important (declaration-no-important)',
@@ -37,6 +38,7 @@ describe('flags errors with invalid css', () => {
 		expect(result.results[0].warnings.map((w) => w.rule)).toEqual([
 			'plugin/stylelint-bem-namics',
 			'plugin/stylelint-bem-namics',
+			'color-function-notation',
 			'color-hex-case',
 			'color-named',
 			'declaration-no-important',
@@ -53,14 +55,16 @@ describe('flags errors with invalid css', () => {
 	});
 
 	it('correct line number', () => {
-		expect(result.results[0].warnings[0].line).toBe(11);
-		expect(result.results[0].warnings[1].line).toBe(13);
-		expect(result.results[0].warnings[2].line).toBe(5);
+		expect(result.results[0].warnings[0].line).toBe(12);
+		expect(result.results[0].warnings[1].line).toBe(14);
+		expect(result.results[0].warnings[2].line).toBe(7);
+		expect(result.results[0].warnings[3].line).toBe(5);
 	});
 
 	it('correct column number', () => {
 		expect(result.results[0].warnings[0].column).toBe(1);
 		expect(result.results[0].warnings[1].column).toBe(1);
-		expect(result.results[0].warnings[2].column).toBe(20);
+		expect(result.results[0].warnings[2].column).toBe(22);
+		expect(result.results[0].warnings[3].column).toBe(20);
 	});
 });
